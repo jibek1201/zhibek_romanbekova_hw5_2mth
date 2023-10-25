6832907cc445b8146451a9bcdfa4ddda5b73df91
jibek12 <romanbekovazibek@gmail.com>
2023-10-25 17:59:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/zhibek_romanbekova_hw5_2mth/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zhibek_romanbekova_hw5_2mth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "zhibek_romanbekova_hw5_2mth"
+include(":app")
+ 
